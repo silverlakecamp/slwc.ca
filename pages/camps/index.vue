be0b0cb44896d2cad79f67bb5dd329a05bb9e7ae
David Mosher <davidmosher@gmail.com>
2019-01-05 20:58:10 -0500

    update camps page, resize images

--- pages/camps/index.vue
+++ pages/camps/index.vue
@@ -1,93 +1,118 @@
 <template>
   <div>
-    <v-layout row wrap>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/1.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/2.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/3.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/4.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/5.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/6.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-carousel height="300px" hide-delimiters>
+      <v-carousel-item
+        v-for="(item,i) in carousel_items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
     <v-container>
-      <v-layout row wrap>
-        <v-flex md8>
-          <h1>This is the <em>Camps</em> page</h1>
-          <br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis explicabo praesentium voluptatem blanditiis accusamus quisquam soluta vel reprehenderit, deserunt, modi consequatur nihil, harum inventore laborum ducimus. Sit vitae in ex.</p>
-          <p>Enim voluptate nesciunt corrupti, repellendus aliquid ea est facilis, recusandae beatae totam, quia iusto dolor ut fugiat quod nisi dicta iste. Aperiam modi, molestiae hic culpa ad libero, officia quibusdam.</p>
-          <p>Delectus maxime, laboriosam ex esse facilis dignissimos. Vitae minus dicta dignissimos. Ducimus laborum quas repellat nemo illum placeat fuga quaerat, vero ipsa dolor sint illo corporis explicabo, quasi voluptatem architecto?</p>
-          <p>Molestias optio officiis aliquam debitis quam nihil, ipsum est provident maiores quos nam soluta dolorum cumque libero, sit dolor distinctio rerum temporibus quis quisquam velit, necessitatibus numquam, rem. Minus, modi!</p>
-          <p>Pariatur temporibus harum minus voluptas, laboriosam earum ex reiciendis aperiam veritatis. Laboriosam, magnam, enim? Adipisci, deleniti incidunt maiores sequi, fugit nobis tempore esse! Blanditiis est ea repellendus, qui corporis quibusdam.</p>
-          <p>Ipsam quibusdam alias totam quas deserunt tenetur deleniti culpa atque magnam quasi aut officia laboriosam quis sunt expedita nobis harum, reiciendis. Dolore tempora eligendi est voluptates, cupiditate itaque cum blanditiis!</p>
-          <p>Magni quae, quibusdam ex, dignissimos eligendi, obcaecati, minima molestias voluptatum ad et iste excepturi repellendus omnis similique! Dolores ducimus, porro quae cupiditate nulla, delectus iusto assumenda molestias necessitatibus eaque perferendis.</p>
-          <p>Minima maxime ullam ad consequuntur illum perspiciatis odio porro, recusandae debitis nisi optio libero repellat a hic eaque ab architecto eum provident nam autem eligendi tempore ex. Culpa, dolore amet?</p>
-          <p>Dicta nisi praesentium sint accusantium fugiat eos, repudiandae dignissimos, unde excepturi vitae, possimus labore. Odio error quos, nam obcaecati possimus rem illum numquam eum excepturi iste enim, architecto sint, accusantium.</p>
-          <p>Beatae impedit nam consectetur porro sit necessitatibus, est aliquam omnis sint unde illo explicabo eveniet, sunt, distinctio vel optio praesentium quas quo commodi recusandae tempora tempore. Quia enim, nihil nobis!</p>
-        </v-flex>
-        <v-flex md3 offset-md1>
-          <v-list two-line>
-            <v-subheader>Sub Navigation</v-subheader>
-            <v-divider></v-divider>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 1</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 2</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 3</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 4</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 5</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-card hover>
-            <v-img src="https://unsplash.it/250/250?image" height="200px"></v-img>
+      <h1>2019 Camps</h1>
+    </v-container>
+    <v-container grid-list-xl>
+      <v-layout row wrap justify-space-between>
+        <v-flex v-for="(camp, i) in camps" :key="i">
+          <v-card>
+            <v-img height="200px" :src="camp.image" />
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0" v-text="camp.title" />
+                <span class="grey--text" v-text="camp.dates" />
+                <div v-text="camp.description" />
+              </div>
+            </v-card-title>
+
+           <v-card-actions>
+             <v-btn flat large color="green">Register!</v-btn>
+           </v-card-actions>
           </v-card>
         </v-flex>
       </v-layout>
     </v-container>
   </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        carousel_items: Array.from(Array(6).keys()).map(i => {
+          return {src: require(`@/assets/img/features/${i+1}.jpg`)}
+        }),
+        camps: [
+          {
+            title: 'Young Adults Retreat',
+            dates: 'May 31 - June 2',
+            description: 'Come join with other young adults for an amazing weekend to kick off our camping season!',
+            for: 'ages 18-28',
+            image: require(`@/assets/img/camps/young-adults.jpg`)
+          },
+          {
+            title: 'Collide Weekend',
+            dates: 'June 7 - 9',
+            description: `An awesome time for students! You won't want to miss it!`,
+            for: 'grades 6-12',
+            image: require(`@/assets/img/camps/collide-weekend.jpg`)
+          },
+          {
+            title: 'Leaders in Training',
+            dates: 'July 1 - 20',
+            description: `For those who want to help in a leadership capacity during the summer of camping.`,
+            for: 'age 14+, limited spots',
+            image: require(`@/assets/img/camps/leaders-in-training.jpg`)
+          },
+          {
+            title: 'Family Camp',
+            dates: 'July 7 - 13',
+            description: `Family fun! Activities! Slides! Watersports... and lots more!`,
+            for: 'all ages',
+            image: require(`@/assets/img/camps/family-camp.jpg`)
+          },
+          {
+            title: 'Junior Kids Camp',
+            dates: 'July 14 - 20',
+            description: `For kids! A great first-time camping experience!`,
+            for: 'ages 7-10',
+            image: require(`@/assets/img/camps/junior-kids.jpg`)
+          },
+          {
+            title: 'Youth Camp',
+            dates: 'July 21 - 27',
+            description: `Yoooooouth Caaaaaamp! Be there!`,
+            for: 'grades 6-12',
+            image: require(`@/assets/img/camps/youth-camp.jpg`)
+          },
+          {
+            title: 'Kids Camp 1',
+            dates: 'July 28 - Aug 3',
+            description: `First kids camp of the season. Bring your friends!`,
+            for: 'ages 9-12',
+            image: require(`@/assets/img/camps/kids-camp-1.jpg`)
+          },
+          {
+            title: 'Geezer Camp',
+            dates: 'Aug 5 - 9',
+            description: `For the young at heart! Bring your funnest self!`,
+            for: 'age 60+',
+            image: require('@/assets/img/camps/geezer-camp.jpg')
+          },
+          {
+            title: 'Kids Camp 2',
+            dates: 'Aug 11 - 17',
+            description: `If you didn't have enough fun in the first kids camp, come on out for round two!`,
+            for: 'ages 9-12',
+            image: require(`@/assets/img/camps/kids-camp-2.jpg`)
+          },
+          {
+            title: 'Sports Camp',
+            dates: 'Aug 18 - 24',
+            description: `For the sporty kids! 3-2-1 Break!`,
+            for: 'ages 9-12',
+            image: require(`@/assets/img/camps/sports-camp.jpg`)
+          }
+        ]
+      }
+    }
+  }
+</script>
