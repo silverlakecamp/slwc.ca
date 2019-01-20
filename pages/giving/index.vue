428a1840bbe4ff0dd997e6e576cac7ab6df15c29
David Mosher <davidmosher@gmail.com>
2019-01-19 22:17:09 -0500

    add giving page content

--- pages/giving/index.vue
+++ pages/giving/index.vue
@@ -1,93 +1,80 @@
 <template>
   <div>
-    <v-layout row wrap>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/1.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/2.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/3.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/4.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/5.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card hover flat tile raised>
-          <v-img :src="require('@/assets/img/features/6.jpg')" height="200px"></v-img>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-container>
+      <h1>Payments / Giving @ SLWC</h1>
+    </v-container>
     <v-container>
       <v-layout row wrap>
         <v-flex md8>
-          <h1>This is the <em>Giving</em> page</h1>
+          <p>This page contains all our donation and payment categories, currently processed via PayPal.</p>
+          <h2 id="payments">Payments</h2>
+          <v-container grid-list-xl>
+            <v-layout column wrap>
+              <v-flex v-for="(payment, i) in payments" :key="i">
+                <v-card>
+                  <v-card-title primary-title>
+                    <h3 class="headline mb-0">
+                      <span v-text="payment.title" />
+                    </h3>
+                  </v-card-title>
+                  <v-card-text>
+                    <p v-text="payment.description" />
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <h2 id="giving">Giving</h2>
+          <v-container grid-list-xl>
+            <v-layout column wrap>
+              <v-flex v-for="(giving, i) in giving" :key="i">
+                <v-card>
+                  <v-card-title primary-title>
+                    <h3 class="headline mb-0">
+                      <span v-text="giving.title" />
+                    </h3>
+                  </v-card-title>
+                  <v-card-text>
+                    <p v-text="giving.description" />
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
           <br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis explicabo praesentium voluptatem blanditiis accusamus quisquam soluta vel reprehenderit, deserunt, modi consequatur nihil, harum inventore laborum ducimus. Sit vitae in ex.</p>
-          <p>Enim voluptate nesciunt corrupti, repellendus aliquid ea est facilis, recusandae beatae totam, quia iusto dolor ut fugiat quod nisi dicta iste. Aperiam modi, molestiae hic culpa ad libero, officia quibusdam.</p>
-          <p>Delectus maxime, laboriosam ex esse facilis dignissimos. Vitae minus dicta dignissimos. Ducimus laborum quas repellat nemo illum placeat fuga quaerat, vero ipsa dolor sint illo corporis explicabo, quasi voluptatem architecto?</p>
-          <p>Molestias optio officiis aliquam debitis quam nihil, ipsum est provident maiores quos nam soluta dolorum cumque libero, sit dolor distinctio rerum temporibus quis quisquam velit, necessitatibus numquam, rem. Minus, modi!</p>
-          <p>Pariatur temporibus harum minus voluptas, laboriosam earum ex reiciendis aperiam veritatis. Laboriosam, magnam, enim? Adipisci, deleniti incidunt maiores sequi, fugit nobis tempore esse! Blanditiis est ea repellendus, qui corporis quibusdam.</p>
-          <p>Ipsam quibusdam alias totam quas deserunt tenetur deleniti culpa atque magnam quasi aut officia laboriosam quis sunt expedita nobis harum, reiciendis. Dolore tempora eligendi est voluptates, cupiditate itaque cum blanditiis!</p>
-          <p>Magni quae, quibusdam ex, dignissimos eligendi, obcaecati, minima molestias voluptatum ad et iste excepturi repellendus omnis similique! Dolores ducimus, porro quae cupiditate nulla, delectus iusto assumenda molestias necessitatibus eaque perferendis.</p>
-          <p>Minima maxime ullam ad consequuntur illum perspiciatis odio porro, recusandae debitis nisi optio libero repellat a hic eaque ab architecto eum provident nam autem eligendi tempore ex. Culpa, dolore amet?</p>
-          <p>Dicta nisi praesentium sint accusantium fugiat eos, repudiandae dignissimos, unde excepturi vitae, possimus labore. Odio error quos, nam obcaecati possimus rem illum numquam eum excepturi iste enim, architecto sint, accusantium.</p>
-          <p>Beatae impedit nam consectetur porro sit necessitatibus, est aliquam omnis sint unde illo explicabo eveniet, sunt, distinctio vel optio praesentium quas quo commodi recusandae tempora tempore. Quia enim, nihil nobis!</p>
         </v-flex>
-        <v-flex md3 offset-md1>
-          <v-list two-line>
-            <v-subheader>Sub Navigation</v-subheader>
-            <v-divider></v-divider>
+        <v-flex md3 offset-lg1>
+          <CampContactCard />
+          <br />
+          <v-list class="elevation-5">
+            <v-subheader>Documents</v-subheader>
+            <v-divider inset></v-divider>
             <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">picture_as_pdf</v-icon>
+              </v-list-tile-action>
               <v-list-tile-content>
-                <v-list-tile-title>Sub Link 1</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 2</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 3</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 4</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>Sub Link 5</v-list-tile-title>
-                <v-list-tile-content>Some Other Stuff</v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="/files/auto-debit-donation-form.pdf">Auto Debit Donation Form</a>
+                </v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
           </v-list>
-          <v-card hover>
-            <v-img src="https://unsplash.it/250/250?image" height="200px"></v-img>
-          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
   </div>
 </template>
+
+<script>
+  import CampContactCard from '~/components/camp-contact-card.vue';
+  export default {
+    components: {CampContactCard},
+    data () {
+      return {
+        giving: this.$store.state.giving.giving,
+        payments: this.$store.state.giving.payments,
+      }
+    }
+  }
+</script>
---
+++ store/giving.js
@@ -0,0 +1,49 @@
+export const state = () => ({
+  payments: [
+    {
+      title: 'Cottage/Trailer Payment',
+      description: 'Payments for cottage/trailer lease fees.',
+      paypal_form_id: '69TQRHFB387VN'
+    }
+  ],
+  giving: [
+    {
+      title: 'Re-new-all Capital Campaign',
+      description: `Re•new•all is our capital campaign that will 'renew' and enhance the major facilities, and ultimately the ministry, over a multi-phase plan in the years to come.`,
+      paypal_form_id: 'M8FAFYDG8ESLN'
+    },
+    {
+      title: 'Water Slide',
+      description: `<p>Wow – What began as a dream in the summer of 2012 finally arrived in 2016. The waterslide was dedicated in May 2016 and has been getting plenty of use since. It is a great attraction for kids and various age groups - a number of geezers have used it.  Our goal in purchasing this slide was to attract more people to the camp, recognizing that once they get here we continue to participate in making and shaping them into Disciples of Christ.  We thank the Lord for providing this tool to further His ministry here at Silver Lake!</p>
+
+      <p>Over the past six years, a total of $54,925.64 was donated towards the slide.  As the final cost of the slide was $80,713, we used funds out of the operational budget to complete the work.  Donations are still being directed towards the shortfall of $25,788.  We invite you to help us make up this shortfall and ask you to consider donating the cost of 1 inch ($58.00), 1 foot ($701) or another amount as you determine.</p>
+      `,
+      paypal_form_id: 'BDWQ5BLLBG7H6'
+    },
+    {
+      title: 'General Operations',
+      description: `At SLWC we try very hard to manage our income streams so that we have the funds necessary to maintain the property, buildings, equipment and infrastructure and to pay the salary of the staff who direct the ministry. Our main income streams are Camper Registration, Retreat and Rental Bookings, Leaseholder Fees and Donations. Approximately 30% of our total income depends on generous people like you...thank you for giving!
+      `,
+      paypal_form_id: '2HLDPEAVNQDGC'
+    },
+    {
+      title: 'Camper Sponsorship',
+      description: `Our passion is to make camping an accessible experience for every child. Some families cannot pay the regis- tration fee, especially if there are several children in the family. Your donation of all (tuition costs between $340-395 typically) or a portion of a camper's registration will make all the difference for some deserving camper.
+      `,
+      paypal_form_id: 'NFHP9ZKUDW54W'
+    },
+    {
+      title: 'Staff Sponsorship',
+      description: `It takes a lot of passionate and energetic staff to minister at camp each summer. Counsellors, activities leaders, office staff, life guards, etc. who give many hours of dedicated time with little pay. We provide a weekly stipend plus room and board and challenge them to raise additional support. Your contribution helps us honour their ministry efforts in the fulfillment of our mission to reach the lost and teach the found. Thank-you for your thoughtful generosity.
+      `,
+      paypal_form_id: 'XPT3XHPGQASGY'
+    },
+    {
+      title: 'Auto Debit Donation',
+      description: `One of the simplest, most thoughtful and helpful means to support our camping ministry is through a regular auto debit donation. You determine the amount, the target to which you give,  and the frequency of your giving. Simply download, print, complete and sign the form. Email or mailing to the listed camp address and you are done. Thank you for your generosity!  Hint: Many of our SLWC Leaseholders care for their annual fees using this method.`,
+      form_url: ''
+    }
+  ]
+})
+
+export const mutations = {}
