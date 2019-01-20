45510fd08944aa789587c2a185b83e195eba86ee
David Mosher <davidmosher@gmail.com>
2019-01-19 23:19:32 -0500

    history and policy pages

---
+++ pages/policy/index.vue
@@ -0,0 +1,207 @@
+<template>
+  <div>
+    <v-container>
+      <h1>Policy @ SLWC</h1>
+    </v-container>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex md8>
+          <h2 id="dogs">Dogs</h2>
+
+          <br>
+
+          <ul>
+            <li>
+              Dogs are welcome on the grounds... so long as they are on a leash and under the control of an adult who cleans up after them.
+            </li>
+            <li>
+              Due to allergies and sanitation issues, we do not permit dogs in accommodations or other public buildings.
+            </li>
+            <li>
+              Dogs are not permitted on the beach.
+            </li>
+            <li>
+              Dogs must have current rabies vaccinations (please have proof available upon request) as per the Department of Health regulation which states: Every operator shall ensure that no animal that is susceptible to rabies is brought into a camp unless the animal has been immunized against rabies at least thirty days and not more than fifteen months prior to the bringing of the animal into the camp. R.R.O. 1990, Reg. 568, s. 7 (3)
+            </li>
+          </ul>
+
+          <br>
+
+          <h2 id="rentals">Rentals</h2>
+
+          <br>
+
+          <p>The Rental Policy was last updated and approved by the board on January 26, 2014</p>
+
+          <v-btn outline flat color="green" href="/files/rental-policy.pdf" target="_blank">
+            Download a copy of the Rental Policy
+          </v-btn>
+
+          <br>
+          <br>
+
+          <h2 id="nutrition">Nutrition</h2>
+
+          <br>
+
+          <p>Although, for the safety of all our campers, <b>NUTS AND NUT PRODUCTS ARE PROHIBITED</b> from our programs and facilities we cannot guarantee that your child will not come in contact with nuts or nut products while here at camp.</p>
+
+          <p>Thank you for helping keep our campers safe.</p>
+
+          <br>
+
+          <h2 id="head-lice">Head Lice</h2>
+
+          <br>
+
+          <p>At Silver Lake Wesleyan Camp we strive to maintain a consistently healthy environment for our campers. As a community living in close quarters, head lice can spread quickly and be hard to eliminate. In an effort to prevent lice, we ask parents to check their child’s head before they come to camp. In order to assist parents and maintain a healthy camp we abide by the following policy:</p>
+
+          <ul>
+            <li>Upon check-in on the first day of camp each camper will be checked for head lice.</li>
+            <li>If any lice or nits are found at that time or at any time during the camping session, the camper will be asked to return home.</li>
+            <li>
+              Once the camper is lice and nit free, they will have the following options:
+              <ul>
+                <li>Choose another week – Parents can bring their child back for another week of equal value during the current summer, if space is available.</li>
+                <li>In the event another week is not available you may choose a refund (two-thirds of the fee)</li>
+                <li>Apply 2/3 of the current year fee towards your child’s camp the following season.</li>
+              </ul>
+            </li>
+          </ul>
+
+          <br>
+
+          <h2 id="refunds">Refunds</h2>
+
+          <br>
+
+          <p>Here at SLWC we understand that life happens, plans change and circumstances beyond your control may necessitate the cancellation of your camp registration. We also realize that budgets are built, staff hired and supplies purchased based on registrations, all of which are impacted by cancellations. So that there are no surprises or misunderstandings here is what happens in the case of cancellation.</p>
+
+          <h3>Full Refund</h3>
+
+          <br>
+
+          <ul>
+            <li>If your selected camp session(s) is full and no alternative can be found or if the financial aid amount requested is not fully granted, the total amount paid to the camp will be refunded (less admin fee of $12.00)</li>
+          </ul>
+
+          <br>
+
+          <h3>Partial Refund</h3>
+
+          <br>
+
+          <p>The balance of the camp fee (less admin fee of $12.00) will be refunded IF:</p>
+
+          <ul>
+            <li>Family plans change and the camper can no longer attend. In this case written notification must be received according to the timeframe on the chart below.</li>
+            <li>The camper does not attend due to illness or injury. A letter from a physician must accompany the refund request.</li>
+            <li>The camper is sent home from camp due to illness or injury occurring at camp.</li>
+            <li>Refunds are prorated in such cases and will not be made for less than half the camp session and will be made only at the written request of the parent/guardian.</li>
+            <li>No refunds will be made if the camper does not attend his/her camp program or leaves early for any other reason, even if the space is filled by another participant.</li>
+          </ul>
+
+          <br>
+
+          <p> We reserve the right to grant exceptions to the stated policy in situations including, but not limited to, illness and death in the immediate family.</p>
+
+          <h3>Time of Request Refund</h3>
+
+          <br>
+
+          <ul>
+            <li>At least 15 days before start of the camp <b>100%</b></li>
+            <li>7 to 14 days before start of the camp <b>50%</b></li>
+            <li>Less than 7 days before start of the camp <b>25%</b></li>
+            <li>After the start of the camp <b>No Refund</b></li>
+          </ul>
+
+          <br>
+
+          <h3 id="drinking-water-safety">Drinking Water Safety</h3>
+
+          <br>
+
+          <p>SLWC holds to a very strict drinking water treatment, monitoring and testing regimine which ensures a safe drinking water supply for all our campers and guests.</p>
+
+          <p>The following information and documents are available at the SLWC office for review by the public free of charge during normal business hours.</p>
+
+          <ul>
+            <li>All test results</li>
+            <li>Annual Reports</li>
+            <li>Trained Person certificates</li>
+            <li>EER and Declaration of Professional Engineer</li>
+            <li>Copy of O. Reg. 170/3</li>
+          </ul>
+
+        </v-flex>
+        <v-flex md3 offset-lg1>
+          <v-list class="elevation-5">
+            <v-subheader>Policy Sections</v-subheader>
+            <v-divider inset></v-divider>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="#dogs">Dogs</a>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="#rentals">Rentals</a>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="#nutrition">Nutrition</a>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="#head-lice">Head Lice</a>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="#refunds">Refunds</a>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="gray">keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <a href="#drinking-water-safety">Drinking Water Safety</a>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
