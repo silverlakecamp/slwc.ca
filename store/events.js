6386ae0aaa0cf073f0f2bfcadda53d85ee6d9115
David Mosher <davidmosher@gmail.com>
2020-01-11 14:37:22 -0500

    add family day 2020 stuff

--- store/events.js
+++ store/events.js
@@ -1,54 +1,55 @@
 export const state = () => ({
   events: [
-    {
-      title: 'Bikers for Christ - Rally for the Son',
-      description: `Love motorcycles? This event is for you! Two days at SLWC with rides, special guest speakers, good food and more!`,
-      dates: 'June 14th to 16th - 2019',
-      times: '9:00am to 5:00pm',
-      image: require(`@/assets/img/events/rally-for-the-son.png`),
-      schedule: null,
-      activities: null,
-      meta: [
-        'Bikers for Christ',
-        'International Elder',
-        'Joey Beldon Collins "Chief"'
-      ],
-      externalContacts: [
-        'Don McGlashing',
-        'seobikersforchrist@hotmail.com',
-        '613-922-7071'
-      ],
-      externalMediaUrl: '/events/2019-rally-for-the-son-poster.pdf',
-      requiresRegistration: false,
-      emailForInfo: true,
-      emailForInfoUrl: 'mailto:office@slwc.ca?subject=Biker%20Rally%20Info',
-      registrationUrl: 'https://campscui.active.com/orgs/SilverLakeWesleyanCamp#/selectSessions/2607807'
-    },
+    // {
+    //   title: 'Bikers for Christ - Rally for the Son',
+    //   description: `Love motorcycles? This event is for you! Two days at SLWC with rides, special guest speakers, good food and more!`,
+    //   dates: 'June 14th to 16th - 2019',
+    //   times: '9:00am to 5:00pm',
+    //   image: require(`@/assets/img/events/rally-for-the-son.png`),
+    //   schedule: null,
+    //   activities: null,
+    //   meta: [
+    //     'Bikers for Christ',
+    //     'International Elder',
+    //     'Joey Beldon Collins "Chief"'
+    //   ],
+    //   externalContacts: [
+    //     'Don McGlashing',
+    //     'seobikersforchrist@hotmail.com',
+    //     '613-922-7071'
+    //   ],
+    //   externalMediaUrl: '/events/2019-rally-for-the-son-poster.pdf',
+    //   requiresRegistration: false,
+    //   emailForInfo: true,
+    //   emailForInfoUrl: 'mailto:office@slwc.ca?subject=Biker%20Rally%20Info',
+    //   registrationUrl: 'https://campscui.active.com/orgs/SilverLakeWesleyanCamp#/selectSessions/2607807'
+    // },
     {
       title: 'Family Day at the Lake',
       description: `Spend a day in the beautiful scenery of frozen Silver Lake with family and friends. There is something for everyone.`,
-      dates: 'Monday, February 18th - 2019',
+      dates: 'Monday, February 17th - 2020',
       times: '9:00am to 5:00pm',
-      image: require(`@/assets/img/events/family-day.jpg`),
+      image: require(`@/assets/img/events/family-day-2020.jpg`),
       schedule: [
-        'Lunch - 11:00am to 2:00pm',
-        'Supper - 4:00pm'
+        'Lunch - 11:00am to 2:00pm'
       ],
       activities: [
+        'Wagon Rides',
         'Dog Sleds',
-        'Sleigh Rides',
-        'Hockey*',
-        'Ice Fishing*',
         'Sledding*',
-        'Snow Shoeing*',
+        'Ice Hockey*',
+        'Maple Taffy Pull',
+        'Axe Throwing',
         'Skating*',
-        'Cross Country Skiing*'
+        'Ice Fishing*',
+        'Great Food'
       ],
       meta: [
+        'Cost: $12/person or $39/family',
         '*bring your own equipment'
       ],
-      requiresRegistration: false,
-      registrationUrl: 'https://www.eventbrite.ca/e/family-day-2019-tickets-54995373643'
+      requiresRegistration: true,
+      registrationUrl: 'https://www.eventbrite.ca/e/family-day-2020-tickets-85106282181'
     },
     {
       title: 'Spring Work Bee',
