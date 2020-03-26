2d3b560a64ceb843695a4df561afc53a276ade04
David Mosher <davidmosher@gmail.com>
2020-03-26 11:14:20 -0400

    add covid response page

---
+++ pages/covid/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-container>
+      <h1>COVID-19 Response</h1>
+    </v-container>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex md12>
+          <p>
+            As you know there is much concern surrounding the COVID-19 situation. While we are uncertain of the full impact of the outbreak on our summer camps, we are proceeding as if our summer camps will be up and running. We will continue to keep you updated as further information becomes available and we encourage everyone to stay informed as the situation develops.
+          </p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
