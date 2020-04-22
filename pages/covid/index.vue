8dec4b69bbd5d3798711fa3d74052e7231d4a5b0
David Mosher <davidmosher@gmail.com>
2020-04-22 12:26:49 -0400

    april 22 covid update

--- pages/covid/index.vue
+++ pages/covid/index.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
     <v-container>
-      <h1>COVID-19 Response</h1>
+      <h1>COVID-19 Response : April 22, 2020 Update</h1>
     </v-container>
     <v-container>
       <v-layout row wrap>
         <v-flex md12>
           <p>
-            As you know there is much concern surrounding the COVID-19 situation. While we are uncertain of the full impact of the outbreak on our summer camps, we are proceeding as if our summer camps will be up and running. We will continue to keep you updated as further information becomes available and we encourage everyone to stay informed as the situation develops.
+           The Camp Board is scheduled to meet every two weeks during the present pandemic. At our <b>April 18, 2020</b>, the following steps were affirmed.
+          </p>
+
+          <p>
+            <b>Summer Camps</b> - As we don’t know yet what the full impact that COVID-19 might have on our summer camps, at present we have not made any changes to our summer camping programs hoping that we can run our full slate of summer camps. Nevertheless, given the uncertainty, we have let our summer staff know that should they seek and find alternative employment, we would release them from their commitment to work at the camp.
+          </p>
+          <p>
+            <b>Rentals</b> - We continue to plan to be able to host our summer rental guests. Nevertheless, given the uncertainty, we have let our rental groups know that we would require only two weeks notice should they decide to cancel their booking.
+          </p>
+          <p>
+            <b>Camp water system</b> - As you know, we typically have our camp water system up and running by the May long weekend (at the latest). Given the current context, while our water system may be operational, we have not yet determined when we might be able to distribute water to the various areas of our camp (we have the ability to turn on or off different sections of the camp).
+            <p>
+              <ul>
+                <li>
+                  <p><b>Trailer sites</b> - As we understand the regulation, our trailer sites must remain closed unless the trailer is the principal residence of the individual. For that reason, under the current regulations we cannot distribute water to our trailer sites.</p>
+                </li>
+                <li>
+                  <p><b>Cottage Sites</b> - As you know, the Premier of Ontario, Doug Ford has requested that cottage owners not travel to their cottages. While we understand that this request made by the Premier is not governed by regulation, we believe it is important to respect this request. For that reason, we will be looking for direction form the various levels of Government before we plan to distribute water to our cottage owners.</p>
+                </li>
+              </ul>
+            </p>
+          </p>
+          <p>
+            <b>Next update</b> - Our camp Board meets next on <b>May 1, 2020</b>. Look for an update on our website shortly
+            after that date.
           </p>
         </v-flex>
       </v-layout>
