5784fe4107e280ad11b59698b5f1c216fc39f577
slwcoffice <81475316+slwcoffice@users.noreply.github.com>
2021-06-03 07:50:24 -0400

    Update index.vue

--- pages/covid/index.vue
+++ pages/covid/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-container>
-      <h1>COVID-19 Response : May 4, 2020</h1>
+      <h1>COVID-19 Response : June 3, 2021</h1>
     </v-container>
     <v-container>
       <v-layout row wrap>
@@ -9,45 +9,18 @@
           <h3>May 4, 2020 Update</h3>
           <hr>
           <p>
-            <b>Young Adult Retreat & Collide Weekend</b> -  It is with heavy hearts that the SLWC Board has decided that due to current COVID-19 pandemic we will be cancelling Young Adult Retreat 2020 (June 5-7) and Collide Weekend 2020 (June 12-14). Though we are saddened that we cannot be together this year for these two amazing events at the lake we are hopeful and look forward to bringing them back better than ever in 2021.
-          </p>
+            <b>As you know, the Provincial authorities recently announced that overnight and days camps would be permitted to run during the summer of 2021 in Ontario.  Given that announcement, we have opened registration for all of our overnight and day camps at slwc.ca/camps We are excited about what the summer will hold for us.
 
-          <p>
-            <b>SLWC Annual Meeting</b> - The SLWC Annual Meeting which is traditionally held on the May long weekend has been postponed. When we are able to reschedule the date we will let you know.
-          </p>
 
-          <p>
-            <b>Sunday Services</b> - Until further notice all Sunday services at SLWC are cancelled.
-          </p>
 
-          <h3>April 22, 2020 Update</h3>
-          <hr>
-          <p>
-           The Camp Board is scheduled to meet every two weeks during the present pandemic. At our <b>April 18, 2020</b>, the following steps were affirmed.
-          </p>
+COVID-19 Protocols - During the summer of 2020, we ran 14 day camps during which we executed a number of COVID-19 protocols.  We are happy to report that we hosted over 300 campers and their families, and that we were able to do so without a single COVID-19 incidence at the camp.  For the upcoming summer of 2021, the Province has issued us preliminary guidelines to follow applicable to COVID-19.  We will work with the Provincial Health authorities to gain the approval of our COVID-19 protocols in the coming days.  Once the Province has signed off on our plan, we will make details of the plan public.
 
-          <p>
-            <b>Summer Camps</b> - As we don’t know yet what the full impact that COVID-19 might have on our summer camps, at present we have not made any changes to our summer camping programs hoping that we can run our full slate of summer camps. Nevertheless, given the uncertainty, we have let our summer staff know that should they seek and find alternative employment, we would release them from their commitment to work at the camp.
-          </p>
-          <p>
-            <b>Rentals</b> - We continue to plan to be able to host our summer rental guests. Nevertheless, given the uncertainty, we have let our rental groups know that we would require only two weeks notice should they decide to cancel their booking.
-          </p>
-          <p>
-            <b>Camp water system</b> - As you know, we typically have our camp water system up and running by the May long weekend (at the latest). Given the current context, while our water system may be operational, we have not yet determined when we might be able to distribute water to the various areas of our camp (we have the ability to turn on or off different sections of the camp).
-            <p>
-              <ul>
-                <li>
-                  <p><b>Trailer sites</b> - As we understand the regulation, our trailer sites must remain closed unless the trailer is the principal residence of the individual. For that reason, under the current regulations we cannot distribute water to our trailer sites.</p>
-                </li>
-                <li>
-                  <p><b>Cottage Sites</b> - As you know, the Premier of Ontario, Doug Ford has requested that cottage owners not travel to their cottages. While we understand that this request made by the Premier is not governed by regulation, we believe it is important to respect this request. For that reason, we will be looking for direction from the various levels of Government before we plan to distribute water to our cottage owners.</p>
-                </li>
-              </ul>
-            </p>
-          </p>
-          <p>
-            <b>Next update</b> - Our camp Board meets next on <b>May 1, 2020</b>. Look for an update on our website shortly
-            after that date.
+
+
+Looking forward to reconnecting with our camper and guest communities in the coming months.
+
+
+          
           </p>
         </v-flex>
       </v-layout>
