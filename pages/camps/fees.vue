f7c8503f968bb98ff986b6f6fa903f317b29141d
David Mosher <davidmosher@gmail.com>
2022-04-02 12:11:03 -0400

    add camp fees page and updated links:

---
+++ pages/camps/fees.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-container>
+      <h1>CAMP FEES & FINANCIAL HELP</h1>
+    </v-container>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex md12>
+          <h2>Camper Fees</h2>
+          <p>Silver Lake Wesleyan Camp’s mission is to share the love of Jesus with as many as possible through camping, retreat and leaseholder Ministries.  For that reason, we work hard to ensure our pricing does not prevent anyone from attending camp that might want to attend.</p>
+
+          <p>Through generous donations, we are able to offer camp fees at less than what the cost to us is.  For 2021, our cost per camper for a week-long camp was $858.00.  By applying the generous donations towards the cost, we were able to offer week long camp fees at between $450 and $500. </p>
+
+          <p>For 2022, we are anticipating the cost to us for a week-long camp to be not less than $900.  For that reason, fees for 2022 were raised so that they would be between $550 and $600 per camper.</p>
+
+          <p>For some folks, these discounted fees may still not be adequate enough to help them send their child, youth or teen to camp.  For those folks, know that we have the ability to access funds that may help them.  If you would like to know more about such assistance, please contact the office at <a href="mailto:office@slwc.ca">office@slwc.ca</a>.</p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
