f473004e27e70ca73a724cdad3437defc4efcd48
slwcoffice <81475316+slwcoffice@users.noreply.github.com>
2022-05-30 17:07:01 -0400

    Update index.vue
    2022 update info

--- pages/covid/index.vue
+++ pages/covid/index.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <v-container>
-      <h1>COVID-19 Response : 2021</h1>
+      <h1>COVID-19 Response : 2022</h1>
     </v-container>
     <v-container>
       <v-layout row wrap>
         <v-flex md12>
-          <h3>June 25, 2021 Update</h3>
+          
           <hr>
           <p>
-            <p>We are excited to report that based on the Province of Ontario guidelines, we are able to open both for day and overnight camps for the upcoming summer of 2021.  We have submitted our COVID-19 Safety Plan to the Leeds, Grenville and Lanark District Health Unit, and this past week, they completed their inspection of our camp.  We passed and so we have the go-ahead to open for the summer.
+            <p>We continue to follow any Covid-19 guidelines set by the government of Ontario. 
 
-Furthermore, based on the COVID-19 protocols we plan to put in place, we believe that similar to last year, we can run our summer camps COVID-19 free.  Looking forward to reconnecting with many of our friends.
+If you have any questions or concerns please email the office at office@slwc.ca
 
-Should you wish to have a copy of our COVID-19 Safety Plan, please email the office at office@slwc.ca
+
 
 
 
