6fe72b09f70ac911a75c986539b68f810a62376a
David Mosher <davidmosher@gmail.com>
2022-06-23 18:26:16 -0400

    1000 campers page

---
+++ pages/1000campers.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <v-img cover :src="require('@/assets/img/oneoffs/1000-campers.jpg')"></v-img>
+    <v-container>
+      <h1>1000 CAMPERS CELEBRATION</h1>
+      <v-layout row wrap>
+        <p>
+          Here is some info about a very exciting promotion we're undertaking at Silver Lake Wesleyan Camp. We're
+          calling
+          it our <b>1000 CAMPERS CELEBRATION</b> and it's exactly what the title says.
+        </p>
+
+        <p>
+          But first, a little back story. Back in as late as 2007 the camp was struggling to keep itself afloat.
+          Registrations were down, leaseholder engagement was down and rentals were practically non existent. With right
+          around 200 campers for the entire season that year (222 to be exact) the discussion was starting to be had as
+          to
+          whether camp ministry (as a whole, or simply at SLWC) had run it's course and needed to “put out to pasture”
+          as
+          they say. There was some serious discussion about the the future of Silver Lake Wesleyan Camp and it's
+          position,
+          and value, in the Central Canada District.
+        </p>
+
+        <p>
+          Well, since then, God has been up to some things. Since 2007 we have experienced steady growth in all the
+          areas
+          of the camp and over the last 5 years, this growth has become exponential. For today, we'll focus just on our
+          camper registrations. In 2017, 10 year's after we were at 222 campers we found ourselves with just over 400
+          campers (409). Then in 2018 we grew to over 500 campers. In 2019 we had over 600 campers (you can see where
+          this
+          is going). 2020 hit and COVID-19 had a drastic effect on us all. But we changed things up and started day
+          camps
+          with over 300 attending, many with their families, that summer. In 2021 the growth did not stop and we were
+          excited to welcome over 800 campers for the first time at SLWC. In fact we had 254 registered for our Collide
+          Weekend alone (moved to September, due to COVID cancellations in June), which is more campers than we had the
+          entire summer of 2007.
+        </p>
+
+        <v-img :src="require('@/assets/img/oneoffs/1000-collage.jpg')"></v-img>
+
+        <p>
+          So… have we set the picture up for you enough? Have we gotten you excited about what God is up to at SLWC.
+          Well
+          we hope so, cause here it is. In honour of our <b>1000 CAMPERS CELEBRATION</b> we are going to give away <b>
+            FREE CAMP FOR
+            LIFE!!!!
+          </b>
+        </p>
+
+        <p>
+          One lucky winner is going to get <b>CAMP FOR LIFE</b>, but that's not all, a whole bunch of other 2nd, 3rd and
+          even
+          4th
+          prize winner will get smaller (still pretty awesome) prizes. And here is how it's going to work. All you have
+          to
+          do to enter into our <b>GRAND PRIZE DRAW</b> is to register for camp. Then, when we hit 1000 campers we are
+          going to
+          draw from that list of 1000 campers for one person to win <b>FREE CAMP FOR LIFE</b>.
+        </p>
+
+        <p>
+          <b>CAMP FOR LIFE</b> will include one registration each season for the camp of the winners choice up to and
+          including
+          Youth Camp. This prize will not include Geezer Camp, Family Camp nor our Spring or Fall Retreats (though
+          registrations for those do include you to win, and they also include you to win any of our 2nd, 3rd (and did
+          we
+          mention) 4th place prizes. Since the <b>FREE CAMP FOR LIFE</b> will be limited to our kids and youth camps we
+          wanted to
+          make sure the winner gets to enjoy this prize for a while. So we are limiting the entries for the GRAND PRIZE
+          DRAW to campers born in 2018 or after (aka turning 14 years old this year). Don't worry, everyone else will be
+          eligible for our 2nd place prize which is a <b>FREE CAMP</b> for 2023 and that DOES included everyone and can
+          be used
+          for any of our camps or retreats for those winners.
+        </p>
+
+        <p>
+          And it doesn't stop there. While there will be only <b>ONE GRAND PRIZE WINNER</b> of our <b>1000 CAMPERS
+            CELEBRATION</b> there
+          are going to be <b>NINE</b> (that's 9!) <b>2nd prize winners</b> of a <b>FREE CAMP</b> in 2023, there will be
+          <b>FIVE 3rd
+            Prize winners</b>
+          for a <b>FREE SLWC HOODIE</b>, and there will be <b>EIGHT 4th Prize winners of FREE SLWC SWAG PACKS</b>. See
+          what we
+          did there
+          1, 9, 5, 8? SLWC was established back in 1958.
+        </p>
+
+        <v-img :src="require('@/assets/img/oneoffs/1000-freecamp.jpg')"></v-img>
+
+        <p>
+          All you have to do to enter is register for one of our camps this summer. Your registration, will
+          automatically
+          enter you in to our <b>1000 CAMPERS CELEBRATION DRAW</b>. We don't know when we'll draw but it will be if and
+          when we
+          get our 1000th camper registration for this season. And hey, if we don't hit 1000 campers this summer, we'll
+          try
+          again next year. But we REALLY want to do it this summer!
+        </p>
+
+        <p>
+          So pay attention to our social channels as we'll announce when we hit milestones for camps this year. We have
+          already hit 100, 200, 300, 400, 500 and we just passed 600 campers, by the way. Of some note: we have passed
+          all
+          current year over year tracking to this point and are actually ahead of our highest registrations by July 1st,
+          and it's still May! If you haven't already, you can register any time at <nuxt-link to="/give">slwc.ca/camps
+          </nuxt-link> and get your name in our
+          <b>1000 CAMPERS CELEBRATION</b> DRAW.
+        </p>
+
+        <p>
+          Can you tell we're excited? Well, we hope you are too. We hope you'll celebrate with us with our
+          <b>1000 CAMPERS CELEBRATION </b> and our give away of <b>FREE CAMP FOR LIFE</b>.
+          We can't wait to see who wins and we can't wait to share the love of Jesus with all the campers
+          that join us this summer at SLWC.
+        </p>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
