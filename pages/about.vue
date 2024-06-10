e979167d9c9adc080f915e321df38718d191eba7
slwcoffice <81475316+slwcoffice@users.noreply.github.com>
2024-06-10 15:15:30 -0400

    Update about.vue
    June 10 Bernie updates

--- pages/about.vue
+++ pages/about.vue
@@ -11,7 +11,7 @@
 
                     <h2>Who We Are and What We Believe</h2>
                     <p>
-                        We are a group of people that run a Christian Camp near Perth Ontario (about an hour southwest
+                        Silver Lake Wesleyan Camp is a Christian Camp near Perth Ontario (about an hour southwest
                         of Ottawa) that is owned by the Central Canada District of the Wesleyan Church of Canada. You
                         will find more about what we believe and how we aspire to live, by reading about us at:
                         <a href="https://www.wesleyan.org/about/our-beliefs"
@@ -21,19 +21,18 @@
 
                     <h2>What We Do</h2>
                     <p>
-                        We run camps and retreats, and host many other groups who do the same. Our goal is to help
-                        people of all ages grow spiritually, mentally, socially and physically in a fun-filled and safe
-                        place of retreat, adventure, learning and challenge. As a Christian camp, everything we do is
-                        rooted in our Biblical convictions about our relationship to God and with one another. Our love
+                        We run camps and retreats, and host many other groups who do the same. We help
+                        people of all ages grow spiritually in a fun-filled and safe place of retreat, adventure, learning
+                        and challenge.  As a Christian Camp,everythings we do is rooted in our Biblical convictions about our relationship to God and with one another. Our love
                         for God and his son Jesus, motivates us to "Share the Love of Jesus with as many as possible".
                     </p>
 
                     <h2>Our Story</h2>
                     <p>
-                        The property that Silver Lake Wesleyan Camp (SLWC) occupies was purchased in 1957. With the
+                        The Silver Lake Wesleyan Camp (SLWC) was purchased in 1957. Our founders had a
                         dream of creating a place where people could come away from their daily routines to be
-                        introduced to the “Good News” and or grow and mature in their walk with the Lord, several men
-                        led the charge to purchase the 55 acres that are SLWC.
+                        introduced to the “Good News” and or grow and mature in their walk with the Lord. Several families
+                        led the charge to purchase the 55 acres for the formation of SLWC.
                     </p>
                     <p>
                         From the early beginnings of just a few camps run each year starting in 1958, the camp grew and
@@ -42,21 +41,21 @@
                         early years.
                     </p>
                     <p>
-                        While the efforts of those early years had significant impact on the many lives of people who
-                        attended the camp, numerical decline and facility aging set in after that period of initial
+                        While the efforts of those early years had significant impact on the many lives of many people who
+                        attended the camp, numerical decline and aging infrastructure set in after that period of initial
                         excitement and growth.
                     </p>
                     <p>
-                        Starting in the early 2000’s, steps were taken in the expectation that some of the dreams that
-                        had not yet been realized could happen. The attitude was “God is not finished with us yet!”
+                        In the early 2000’s, steps were taken in the expectation that SLWC could be renewed.
+                        The attitude was “God is not finished with us yet!”
                     </p>
                     <p>
                         By 2010, our camper registrations were on the rise and the number of rental groups utilizing our
-                        site were on the increase. From a low of 222 camper registrations in 2004, a record number of
+                        site was on the increase. From a low of 222 camper registrations in 2004, a record number of
                         campers were registered for 2022 (just over 1200.) Equally stunning was the growth of the number
-                        of rental groups that use our facility. From about ½ dozen in the early 2000’s, to a record 27
-                        in 2016. We are currently on track to best that number with 38 groups and events registered for
-                        2024.
+                        of rental groups that use our facility. 
+                        We now have 38 groups per year renting SLWC facilities.
+                        
                     </p>
                     <p>While we have stepped out in faith and done the necessary work, God has granted the increase.</p>
 
@@ -71,41 +70,33 @@
 
                     <h2>Respecting Earthly Authorities</h2>
                     <p>
-                        In more than one place in scripture, Christians are called to submit to the various authorities
-                        God has placed on earth. This is the same God that over and over again in history has
-                        demonstrated the dignity and intrinsic value of people, protection for whom is to be overseen by
-                        Governments.
+                        SLWC is committed to dignity and intrinsic value of people complies with Canadian standards of safety
+                        for people of all ages.
                     </p>
-                    <p>
-                        Specifically, we understand and appreciate the protections the Canadian Constitution affords
-                        religious organisations to practise and live out their beliefs. Accordingly, we clearly
-                        understand the importance of functioning under the many frameworks or “Regimes” that the
-                        Government of Canada (and Provinces) have put in place in an attempt to effectively and fairly
-                        Govern the nation of Canada. While not a complete list, we comply with the requirements of:
+                        SLWC appreciates the protections the Canadian Constitution affords religious organisations to practise and live out their beliefs.
+                        
+                        While not a complete list, SLWC complies with the requirements of:
                     </p>
                     <ul>
                         <li>
-                            <strong>The Taxation Regime</strong> -- As a Registered Charity, we put much effort into
-                            understanding and abiding by the many Federal and Provincial laws and regulations that have
-                            been put in place for Registered Charities. This includes requirements to collect and submit
+                            <strong>Federal and Provincial Taxation Requirements</strong> -- As a Registered Charity, SLWC
+                            abides by Federal and Provincial laws and regulations. This includes requirements to collect and submit
                             HST, to deduct and submit from employee’s pays, Employment Insurance, Canada Pension Plan,
                             Worker’s Compensation and Tax Deducted, and to file information returns reporting on our
                             financial activities.
                         </li>
                         <br />
                         <li>
-                            <strong>The Employment Standards Regime</strong> -- We employ over 50 people throughout the
-                            year. We work hard to understand the rules about and the minimum requirements of employing
-                            people in Canada. That said, our desire is to “exceed” the minimum as we are able to.
-                            Standards we adhere to under this regime fall into the following topics (not an exhaustive
-                            list); vacation Leave, hours of work including specific guidelines for employment at Camps,
+                            <strong>Employment Standards</strong> -- SLWC employs over 50 people throughout the
+                            year. SLWC complies with federal and provincal employment requirements. These include the following topics
+                            (not an exhaustive list); vacation leave, hours of work including specific guidelines for employment at Camps,
                             minimum wage, severance pay and termination requirements, public holidays and maternity and
                             parental leave.
                         </li>
                         <br />
                         <li>
                             <strong>Health and Safety Regime</strong> -- As a camp that welcomes 1000’s of people of all
-                            ages to our site each year, including many children, we work hard to ensure that their stay
+                            ages to our site each year, including many children, SLWC ensures that their stay
                             here will be safe. Provincial authorities provide guidance for the many areas of “Camping”
                             where the safeguarding of people’s well-being is important. Areas covered by these
                             authorities include:
@@ -124,28 +115,27 @@
                         <br />
                         <li>
                             <p>
-                                <strong>The Drinking Water and Septic Systems Regime</strong> -- As we operate what is
-                                considered a small drinking water facility, we follow 22 established protocols to ensure
-                                our drinking water is safe. We treat, test, and measure and record our water usage. We
-                                also measure the amount of waste water we create. We report annually to the Federal
-                                Authorities on such usage.
+                                <strong>Drinking Water and Septic Systems</strong> -- SLWC operates what is
+                                considered a small drinking water facility. The camp follows 22 established protocols to ensure
+                                the safe drinking water. Water is treated, tested, and measured. Water usage is recorded.
+                                Watse water is measured according to government standards and an annual report of waste water
+                                usage is filed.
                             </p>
                             <p>
-                                The camp wide septic solution designed and installed for us that was functioning by
-                                2022, was approved by the Ministry of the Environment and Climate Change authorities. It
-                                demonstrates our care for the Environment.
+                                The camp wide septic solution that launched in 2022 was approved by the Ministry of the Environment
+                                and Climate Change authorities.
                             </p>
                         </li>
                         <li>
                             <p>
-                                <strong>The Food Services and Hospitality Regime</strong> -- Serving upwards of 30,000
-                                meals each year, we ensure that at all times when food is served, we have a staff member
-                                that holds the appropriate food handlers certificate present. We are able to cater to
-                                many specific foods requests, such as dairy and or gluton free. Our menus ensure that
+                                <strong>Food Services and Hospitality</strong> -- Serving upwards of 30,000
+                                meals each year, SLWC ensures that at all times when food is served, a staff member
+                                that holds the appropriate food handlers certificate present. SLWC accommodates
+                                many specific foods requests, such as dairy and or gluton free. Menus ensure that
                                 campers are served “healthy’ foods during their time with us.
                             </p>
                             <p>
-                                Our lodging rooms (dorms, motel style, cabin etc.) are cleaned regularly and inspected
+                                SLWC lodging rooms (dorms, motel style, cabin etc.) are cleaned regularly and inspected
                                 by the health authorities.
                             </p>
                         </li>
@@ -153,35 +143,32 @@
                             <p>
                                 <strong>Building Codes </strong> -- The camp has 23 buildings that help us serve the
                                 1000’s of people that visit us each year. New buildings are constructed to code. For
-                                some of our buildings the requirements are more stringent, as we are located adjacent to
-                                a lake, and the conservation authorities speak to some of our development. Existing
-                                buildings are maintained to ensure guests are safe.
+                                some of our buildings the requirements are more stringent. SLWC located adjacent to
+                                a lake, and the conservation authorities provide consultatioin in certain aspects of infrastructure development.
+                                Existing buildings are maintained to ensure guests are safe.
                             </p>
                         </li>
                         <li>
                             <p>
-                                <strong>The Human Rights Regime</strong> -- With thousands of people visiting our site
-                                every year, we are cognizant of the need to treat all who visit our site with dignity
+                                <strong>Human Rights</strong> -- Thousands of people visiting SLWC are treated with dignity
                                 and respect, recognizing that we live in a world where people have many different
                                 beliefs, perspectives and practices.
                             </p>
                             <p>
-                                At the forefront of our concern is the need to protect the children that are our guests
-                                for a short time. We have developed and implemented a significant Child Protection
+                                At the forefront of our concern is the need to protect the children who participate in camping
+                                programs and activities. SLWC has developed and implemented a significant Child Protection
                                 Policy, to guard against abuse and ensure our staff engage appropriately with our
-                                campers. We take scripture seriously when it warns against leading any “child” astray.
+                                campers. SLWC takes scripture seriously when it warns against leading any “child” astray.
                             </p>
                             <p>
-                                We believe Christians are called to be at the forefront of treating all people with
-                                dignity and respect. For that reason, we aspire to live with the same spirit and
-                                attitudes that led Christians to work toward the abolishment of slavery, that led
-                                Christians to establish Hospitals, and that led Christians to found places of Higher
-                                education.
+                                Christians are called to be at the forefront of treating all people with
+                                dignity and respect. For that reason, SLWC adopts the same spirit and
+                                attitudes that historically led Christians to work toward the abolishment of slavery, establish hospitals
+                                and launch institutions of Higher education.
                             </p>
                             <p>
-                                Perhaps most notable? Our Lord stepped into a world where women were considered second
-                                class. By His actions He countered the prevalent thinking and attitudes toward women,
-                                demonstrating their equal and intrinsic value.
+                                SLWC practices an egalitarian approach towards women and men in leadershp.
+                                
                             </p>
                         </li>
                     </ul>
