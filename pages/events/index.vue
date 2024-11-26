464bd789bf8f6669501abdb1aa4275e29218d6a6
David Mosher <davidmosher@gmail.com>
2024-11-26 09:17:36 -0500

    remove currentYear context from page headers - this used to be used, but only ever updated if the site was rebuilt so we removed it from page headings opting for more generic titles like "Events" instead of "2024 Events"

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,35 +1,44 @@
 <template>
-  <div>
-    <v-carousel height="500px" hide-delimiters>
-      <v-carousel-item
-        v-for="(item,i) in carousel_items"
-        :key="i"
-        :src="item.src"
-      ></v-carousel-item>
-    </v-carousel>
-    <v-container>
-      <h1><span v-text="currentYear" /> Events</h1>
+    <div>
+        <v-carousel height="500px" hide-delimiters>
+            <v-carousel-item v-for="(item, i) in carousel_items" :key="i" :src="item.src"></v-carousel-item>
+        </v-carousel>
+        <v-container>
+            <h1>Events</h1>
 
-      <p>Outside of our seasonal summer camps at SLWC we offer a few annual events as well (non camps). This includes our annual Family Day event in February and our Work Bees in Spring and Fall.  Check this page for all event information. It will be updated as we finalize information. 
+            <p>
+                Outside of our seasonal summer camps at SLWC we offer a few annual events as well (non camps). This
+                includes our annual Family Day event in February and our Work Bees in Spring and Fall. Check this page
+                for all event information. It will be updated as we finalize information.
+            </p>
 
+            <p>
+                Please stay tuned for more information coming soon on the <a href="/camps">camps page</a> with details
+                as we find out more.
+            </p>
 
-
-      <p>Please stay tuned for more information coming soon on the <a href="/camps">camps page</a> with details as we find out more.</p>
-
-      <iframe class="airtable-embed" src="https://airtable.com/embed/shr1On0UdE13fxW1b?backgroundColor=red" frameborder="0" onmousewheel="" width="100%" height="580" style="background: transparent; border: 1px solid #ccc;"></iframe>
-    </v-container>
-  </div>
+            <iframe
+                class="airtable-embed"
+                src="https://airtable.com/embed/shr1On0UdE13fxW1b?backgroundColor=red"
+                frameborder="0"
+                onmousewheel=""
+                width="100%"
+                height="580"
+                style="background: transparent; border: 1px solid #ccc"
+            ></iframe>
+        </v-container>
+    </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        carousel_items: Array.from(Array(6).keys()).map(i => {
-          return {src: require(`@/assets/img/events/carousel/${i+1}.jpg`)}
-        }),
-        events: this.$store.state.events.events
-      }
-    }
-  }
+export default {
+    data() {
+        return {
+            carousel_items: Array.from(Array(6).keys()).map((i) => {
+                return { src: require(`@/assets/img/events/carousel/${i + 1}.jpg`) };
+            }),
+            events: this.$store.state.events.events,
+        };
+    },
+};
 </script>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,30 +1,32 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vue from "vue";
+import Vuetify from "vuetify";
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#b1d235',
-    secondary: '#b1d235',
-    accent: '#b1d235',
-    error: '#b1d235',
-    info: '#b1d235',
-    success: '#b1d235',
-    warning: '#b1d235',
+    primary: "#b1d235",
+    secondary: "#b1d235",
+    accent: "#b1d235",
+    error: "#b1d235",
+    info: "#b1d235",
+    success: "#b1d235",
+    warning: "#b1d235",
   },
 });
 
 Vue.mixin({
-  data: function() {
+  data: function () {
     return {
-      get currentYear() {
-        let d = new Date()
-        // if we're in december, flip the currentYear to the next year for golive
-        if(d.getMonth() == 11) {
-          return d.getFullYear() + 1;
-        } else {
-          return d.getFullYear();
-        }
-      }
-    }
-  }
+      // this used to be used, but only ever updated if the site was rebuilt
+      // so we removed it from page headings opting for more generic titles like "Events" instead of "2024 Events"
+      // get currentYear() {
+      //   let d = new Date()
+      //   // if we're in december, flip the currentYear to the next year for golive
+      //   if(d.getMonth() == 11) {
+      //     return d.getFullYear() + 1;
+      //   } else {
+      //     return d.getFullYear();
+      //   }
+      // }
+    };
+  },
 });
