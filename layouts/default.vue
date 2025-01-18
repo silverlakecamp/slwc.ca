d0b0dd24bb733d27fa257f38fd2f457e5549e9e2
David Mosher <davidmosher@gmail.com>
2025-01-18 14:40:02 -0500

    response/footer improvements

--- layouts/default.vue
+++ layouts/default.vue
@@ -59,10 +59,94 @@
 #app-footer {
   background-color: #777776;
   color: white;
+  padding: 2rem 0;
 }
 
 #app-footer a {
   color: #F1F2F2;
+  text-decoration: none;
+  display: inline-block;
+  padding: 0.5rem 0;
+}
+
+#app-footer .vcard {
+  margin: 1.5rem 0;
+}
+
+#app-footer .vcard .org {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+#app-footer h3 {
+  margin: 1.5rem 0 1rem;
+  font-size: 1.2rem;
+}
+
+#app-footer .about,
+#app-footer .social-media {
+  margin-top: 1.5rem;
+}
+
+#app-footer iframe {
+  width: 100%;
+  height: 100%;
+  min-height: 450px;
+  border-radius: 4px;
+}
+
+.mobile-map {
+  display: none;
+}
+
+@media (max-width: 960px) {
+  #app-footer .white-loon {
+    display: none;
+  }
+
+  #app-footer .vcard,
+  #app-footer .about,
+  #app-footer .social-media {
+    text-align: center;
+  }
+
+  #app-footer a {
+    padding: 0.75rem 0;
+  }
+
+  #app-footer br {
+    display: none;
+  }
+
+  #app-footer .about a,
+  #app-footer .social-media a {
+    display: block;
+    margin: 0.25rem 0;
+  }
+
+  #app-footer .vcard .adr > *:not(:last-child):after {
+    content: ", ";
+  }
+
+  #app-footer .vcard .adr br {
+    display: none;
+  }
+
+  .mobile-map {
+    display: block;
+    margin: 2rem -24px -2rem -24px;
+    position: relative;
+    width: calc(100% + 48px);
+  }
+
+  .mobile-map iframe {
+    width: 100%;
+    height: 300px;
+    border-radius: 0;
+    border: 0;
+    display: block;
+  }
 }
 
 .theme--light.v-toolbar {
@@ -91,7 +175,7 @@
             </nuxt-link>
             <v-toolbar-title v-text="title" hidden />
             <v-spacer></v-spacer>
-            <v-flex text-xs-center style="margin-left: -10%" class="hidden-sm-and-down">
+            <v-flex text-xs-center style="margin-left: -10%">
                 <a href="mailto:info@slwc.ca">info@slwc.ca</a>
                 |
                 <a href="tel:+18775112267">1-877-511-2267</a>
@@ -107,14 +191,15 @@
         <footer id="app-footer">
             <v-container grid-list-xl>
                 <v-layout row wrap>
-                    <v-flex lg2>
+                    <v-flex xs12 md4 lg3>
                         <img class="white-loon" src="~/assets/img/slwc_alt_icon_white.png" :alt="title" />
 
                         <div class="vcard">
                             <div class="org">Silver Lake Wesleyan Camp</div>
                             <div class="adr">
-                                <div class="street-address">512 Wesleyan Camp Lane</div>
-                                <span class="locality">Maberly</span>, <span class="region">ON</span>,
+                                <span class="street-address">512 Wesleyan Camp Lane</span>
+                                <span class="locality">Maberly</span>
+                                <span class="region">ON</span>
                                 <span class="country-name">Canada</span>
                                 <br />
                                 <span class="postal-code">K0H 2B0</span>
@@ -126,7 +211,6 @@
                         </div>
 
                         <div class="about">
-                            <br />
                             <h3>Quick Links</h3>
                             <a href="/about">About SLWC</a>
                             <br />
@@ -142,12 +226,12 @@
                                 class="hidden-md-and-up"
                                 href="https://maps.google.com/maps?ll=44.835455,-76.593231&z=14&t=m&hl=en-US&gl=CA&mapclient=embed&cid=2469503918391799052"
                                 target="_blank"
-                                >Directions</a
                             >
+                                Directions
+                            </a>
                         </div>
 
                         <div class="social-media">
-                            <br />
                             <h3>Social Media</h3>
                             <a href="https://facebook.com/slwc.ca">Facebook</a>
                             <br />
@@ -160,17 +244,22 @@
                             <a href="https://www.vimeo.com/silverlake">Vimeo</a>
                         </div>
                     </v-flex>
-                    <v-flex lg7 hidden-sm-and-down>
+                    <v-flex xs12 md8 lg9 class="d-none d-md-block">
                         <iframe
-                            style="border: 0"
                             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11317.272312879688!2d-76.5932308!3d44.8354553!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x224570bad32dad0c!2sSilver+Lake+Wesleyan+Camp!5e0!3m2!1sen!2sca!4v1473269309127"
-                            frameborder="0"
-                            width="600"
-                            height="450"
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
                         ></iframe>
                     </v-flex>
                 </v-layout>
             </v-container>
+            <div class="mobile-map d-md-none">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11317.272312879688!2d-76.5932308!3d44.8354553!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x224570bad32dad0c!2sSilver+Lake+Wesleyan+Camp!5e0!3m2!1sen!2sca!4v1473269309127"
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
         </footer>
     </v-app>
 </template>
